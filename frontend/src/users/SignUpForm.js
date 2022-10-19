86b452e415b2c8ce8cdea8b4fd51813facf095fc
QuentonPow311 <quenpowell@gmail.com>
2022-10-19 17:02:03 -0400

    d

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -54,17 +54,30 @@ function SignUpForm() {
 						/>
 					</div>
 				</div>
-				<div className="row">
+				  
+<div className="row">
+    <div className="col-sm-6 form-group">
+        <label htmlFor="email">Email</label>
+        <input
+            type="email"
+            required
+            value={user.email}
+            onChange={e => setUser({ ...user, email: e.target.value })}
+            className="form-control"
+            id="email"
+            name="email"
+       		 />
+    			</div>
 					<div className="col-sm-6 form-group">
-						<label htmlFor="email">Email</label>
+						<label htmlFor="password">Password</label>
 						<input
-							type="email"
+							type="password"
 							required
-							value={user.email}
-							onChange={e => setUser({ ...user, email: e.target.value })}
+							value={user.password}
+							onChange={e => setUser({ ...user, password: e.target.value })}
 							className="form-control"
-							id="email"
-							name="email"
+							id="password"
+							name="password"
 						/>
 					</div>
 				</div>
